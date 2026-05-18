736557ca4629231744704227e06f8d0fe6e00084
jxleners <jjelenepunongbayan@gmail.com>
2026-05-19 00:08:40 +0800

    refactored signup, updated dashboard and converted pages to PHP

--- restock.php
+++ restock.php
@@ -6,7 +6,7 @@
 // ============================================================
 session_start();
 if (!isset($_SESSION['user_id'])) {
-    header("Location: index.html");
+    header("Location: index.php");
     exit;
 }
 
